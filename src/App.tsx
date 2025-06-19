47152c94dab4adae9a6b30379f776868967520ae
Gabriel Ribeiro <gabrielgoncalves310503@gmail.com>
2025-06-19 17:51:38 -0300

    clear repository

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,7 @@ import "./styles/reset.scss";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import Layout from "./layouts/Layout";
 import { store } from "./store";
-import Login from "./pages/Login";
-import HelloWorld from "./pages/HelloWorld";
-import Register from "./pages/Register";
-import LayoutInternal from "./layouts/LayoutInternal";
-import StartMembership from "./pages/StartMembership";
-import Profile from "./pages/Profile";
-import MyPlan from "./pages/MyPlan";
-import Upload from "./pages/Upload";
-import Dashboard from "./pages/Dashboard";
-import CMSDashboard from "./pages/CMSDashboard";
 import Search from "./pages/Search";
 import NewLayout from "./layouts/NewLayout";
 import RoomDetails from "./pages/RoomDetails";
@@ -27,72 +16,6 @@ import Confirmation from "./pages/Confirmation";
 
 function App() {
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LayoutInternal />,
-      children: [
-        {
-          path: "hello",
-          element: <HelloWorld />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "upload",
-          element: <Upload />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "old-dashboard",
-          element: <Dashboard />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "dashboard",
-          element: <CMSDashboard />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "profile",
-          element: <Profile />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "plan",
-          element: <MyPlan />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-      ],
-    },
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "login",
-          element: <Login />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "register",
-          element: <Register />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        },
-        {
-          path: "membership",
-          element: <StartMembership />,
-          loader: async () => null,
-          errorElement: <div>erro</div>,
-        }
-      ],
-    },
     {
       path: "/",
       element: <NewLayout />,
